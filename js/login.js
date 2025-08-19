533bf8560374c4acaf6ca3443df54714287b5dc9
London-Grant <londongrant@outlook.com>
2025-08-18 23:34:25 -0400

    Adding TOS

---
+++ js/login.js
@@ -0,0 +1,10 @@
+
+const backend_url_base = "https://charmed-crane-easy.ngrok-free.app/"
+const submit = document.getElementById("submit")
+
+submit.addEventListener('click', function(){
+    user_email = document.getElementById("email")
+    user_password = document.getElementById("password");
+
+    
+})

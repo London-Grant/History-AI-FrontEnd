fe789859cefc2dcf95b35839c32e88fa5a548304
London-Grant <londongrant@outlook.com>
2025-08-20 16:02:30 -0400

    Adding Login API

--- js/login.js
+++ js/login.js
@@ -1,10 +1,15 @@
 
 const backend_url_base = "https://charmed-crane-easy.ngrok-free.app/"
-const submit = document.getElementById("submit")
 
-submit.addEventListener('click', function(){
-    user_email = document.getElementById("email")
-    user_password = document.getElementById("password");
+document.getElementById("tiktokLogin").addEventListener("click", () => {
+    // TODO: Add a function for this in the backend, and then create a config with developer credentials 
+      const clientKey = "sbaw9y0gyp43g6nq3x";
 
-    
-})
+      const redirectUri = encodeURIComponent("https://yourfrontend.com/portal.html"); 
+      const scope = encodeURIComponent("user.info.basic,video.upload");
+      const responseType = "code";
+
+      const authUrl = `https://www.tiktok.com/auth/authorize/?client_key=${clientKey}&scope=${scope}&response_type=${responseType}&redirect_uri=${redirectUri}&state=xyz123`;
+
+      window.location.href = authUrl;
+    });
